ba4b587567a457d947d6d301ec8bd53e664a36a9
Jason R. Jones <slightlynybbled@gmail.com>
2017-05-30 12:03:44 -0400

    Added docstrings, __rsub__() method

--- engineering_notation/engineering_notation.py
+++ engineering_notation/engineering_notation.py
@@ -2,6 +2,10 @@ from decimal import *
 
 
 class EngNumber:
+    """
+    Used for easy manipulation of numbers which use engineering notation
+    """
+
     suffix_lookup = {
         'p': 'e-12',
         'n': 'e-9',
@@ -27,6 +31,12 @@ class EngNumber:
     }
 
     def __init__(self, value: (str, int, float), precision=2):
+        """
+        Initialize the class
+
+        :param value: string, integer, or float representing the numeric value of the number
+        :param precision: the precision past the decimal - default to 2
+        """
         self.precision = precision
 
         if isinstance(value, str):
@@ -43,6 +53,10 @@ class EngNumber:
             self.number = Decimal(str(value))
 
     def __repr__(self):
+        """
+        Returns the string representation
+        :return: a string representing the engineering number
+        """
         # since Decimal class only really converts number that are very small
         # into engineering notation, then we will simply make all number a
         # small number and take advantage of Decimal class
@@ -58,9 +72,18 @@ class EngNumber:
         return base + self.exponent_lookup_scaled[exponent]
 
     def __str__(self, eng=True, context=None):
+        """
+        Returns the string representation
+        :return: a string representing the engineering number
+        """
         return self.__repr__()
 
     def __add__(self, other):
+        """
+        Add two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
@@ -68,16 +91,43 @@ class EngNumber:
         return EngNumber(str(num))
 
     def __radd__(self, other):
+        """
+        Add two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         return self.__add__(other)
 
     def __sub__(self, other):
+        """
+        Subtract two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
         num = self.number - other.number
         return EngNumber(str(num))
 
+    def __rsub__(self, other):
+        """
+        Subtract two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
+        if not isinstance(other, EngNumber):
+            other = EngNumber(other)
+
+        num = other.number - self.number
+        return EngNumber(str(num))
+
     def __mul__(self, other):
+        """
+        Multiply two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
@@ -85,9 +135,19 @@ class EngNumber:
         return EngNumber(str(num))
 
     def __rmul__(self, other):
+        """
+        Multiply two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         return self.__mul__(other)
 
     def __truediv__(self, other):
+        """
+        Divide two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
@@ -95,6 +155,11 @@ class EngNumber:
         return EngNumber(str(num))
 
     def __rtruediv__(self, other):
+        """
+        Divide two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
@@ -102,30 +167,55 @@ class EngNumber:
         return EngNumber(str(num))
 
     def __lt__(self, other):
+        """
+        Compare two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
         return self.number < other.number
 
     def __gt__(self, other):
+        """
+        Compare two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
         return self.number > other.number
 
     def __le__(self, other):
+        """
+        Compare two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
         return self.number <= other.number
 
     def __ge__(self, other):
+        """
+        Compare two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
         return self.number >= other.number
 
     def __eq__(self, other):
+        """
+        Compare two engineering numbers
+        :param other: EngNum, float, or int
+        :return: result
+        """
         if not isinstance(other, EngNumber):
             other = EngNumber(other)
 
