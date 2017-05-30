4326ea700c1a86d567e6b785169a6cd3ca0dfaf8
Jason R. Jones <slightlynybbled@gmail.com>
2017-05-30 15:22:39 -0400

    Updated setup to automatically convert markdown readme to rst

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 from setuptools import setup, find_packages
 import os
 
+# ---------------------------------
+# imports the version from the package
 here = os.path.dirname(os.path.dirname(__file__))
 exec(open(os.path.join(here, 'engineering_notation/version.py')).read())
 
-with open('readme.md', 'r') as f:
-    readme = f.read()
+# ---------------------------------
+# converts the readme.md to readme.rst
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
 
+# ---------------------------------
+# project requirements
 requirements = []
 
+# ---------------------------------
+# project setup
 setup(
     name='engineering_notation',
     version=__version__,
     description='Easy engineering notation',
-    long_description=readme,
+    long_description=read_md('readme.md'),
     author='Jason R. Jones',
     author_email='slightlynybbled@gmail.com',
     url='https://github.com/slightlynybbled/engineering_notation',
